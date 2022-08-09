ce61150c9049874495d029211cc6eadf53862bff
savagebuff <choney@student.21-school.ru>
2022-08-09 21:36:02 +0300

    разложил по папкам

--- PartyHub/PartyHub/Controllers/ViewController.swift
+++ PartyHub/PartyHub/Controllers/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = .systemPink
     }
 
