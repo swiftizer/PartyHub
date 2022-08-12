e69541030e577a238f6af0fb60e5bcf6c36b9e52
JuliaAlfimova <juliemoorled@gmail.com>
2022-08-12 14:47:27 +0300

    fixed creating tabbar items and added animation

--- PartyHub/PartyHub/Controllers/TabBarController.swift
+++ PartyHub/PartyHub/Controllers/TabBarController.swift
@@ -8,59 +8,73 @@
 import UIKit
 import PinLayout
 
-final class TabBarVC: UITabBarController {
-    
-    enum TabBarItem: String, CaseIterable {
-        
-        case list = "Menu"
-        case map = "Map"
-        case profile = "Profile"
-        
-        var viewController: UIViewController {
-            switch self {
-            case .list:
-                return ListVC()
-            case .map:
-                return ListVC() //change VC
-            case .profile:
-                return ListVC() //change VC
-            }
-        }
-        
-        var itemImages: UIImage? {
-            switch self {
-            case .list:
-                return UIImage(systemName: "list.bullet")
-            case .map:
-                return UIImage(systemName: "map")
-            case .profile:
-                return UIImage(systemName: "person")
-            }
+enum TabBarItem: String, CaseIterable {
+
+    case list = "Menu"
+    case map = "Map"
+    case profile = "Profile"
+
+    var itemImages: UIImage? {
+        switch self {
+        case .list:
+            return UIImage(systemName: "list.bullet")
+        case .map:
+            return UIImage(systemName: "map")
+        case .profile:
+            return UIImage(systemName: "person")
         }
     }
+}
+
+class TabBarVCFactory {
+    func createTabBarVC(tabBarItem: TabBarItem) -> UIViewController {
+        switch tabBarItem {
+        case  .list:
+            return ListVC()
+        case .map:
+            return ListVC() //change VC
+        case .profile:
+            return ListVC() //change VC
+        }
+    }
+}
+
+final class TabBarVC: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpTabBar()
         styleTabBar()
+        setUpTabBar()
     }
     
-    private func setUpTabBar() {
-        
-        viewControllers = [generateTabBarItem(tabBarItem: .list),
-                           generateTabBarItem(tabBarItem: .map),
-                           generateTabBarItem(tabBarItem: .profile)]
-    }
-    
-    private func generateTabBarItem(tabBarItem: TabBarItem) -> UIViewController {
-        let viewController = tabBarItem.viewController
-        viewController.tabBarItem.title = tabBarItem.rawValue
-        viewController.tabBarItem.image = tabBarItem.itemImages
-        return viewController
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let imageView = item.value(forKey: "view") as? UIView else { return }
+        imageView.layer.add(bounceAnimation, forKey: nil)
     }
     
     private func styleTabBar() {
         tabBar.backgroundColor = .systemGray6
     }
+    
+    private func setUpTabBar() {
+        let tabBarItems = [TabBarItem.list, TabBarItem.map, TabBarItem.profile]
+        var viewControllers: [UIViewController] = []
+        for item in tabBarItems {
+            let tabBarVCFactory = TabBarVCFactory()
+            let viewController = tabBarVCFactory.createTabBarVC(tabBarItem: item)
+            viewController.tabBarItem.title = item.rawValue
+            viewController.tabBarItem.image = item.itemImages
+            viewControllers.append(viewController)
+        }
+        self.viewControllers = viewControllers
+    }
+    
+    private lazy var bounceAnimation: CAKeyframeAnimation = {
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0, 1.1, 0.9, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(0.3)
+        bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
+        return bounceAnimation
+    }()
     
 }
