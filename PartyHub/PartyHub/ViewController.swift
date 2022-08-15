0195a681701bc8f1ee3f0bffacd2b4806552a96a
Serzh3142001 <78726466+Serzh3142001@users.noreply.github.com>
2022-08-15 16:57:03 +0300

    Revert "Feature/launchscreen"

---
+++ PartyHub/PartyHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyHub
+//
+//  Created by Сергей Николаев on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
