33b2ebbcc70048650084cbbc8098dd15864b7ac7
JuliaAlfimova <juliemoorled@gmail.com>
2022-08-17 19:26:23 +0300

    added tableview adapter

--- PartyHub/PartyHub/Controllers/MenuVC/MenuVC.swift
+++ PartyHub/PartyHub/Controllers/MenuVC/MenuVC.swift
@@ -7,25 +7,21 @@
 
 import UIKit
 
-class MenuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MenuVC: UIViewController {
 
     // MARK: - Private properties
 
-    private struct Cells {
-        static let addCell = "AddTableViewCell"
-        static let menuCell = "MenuTableViewCell"
-    }
-
+    private var adapter: MenuTableViewAdapter!
     private let menuTableView = UITableView()
-
-    private let addCellHeight: CGFloat = 80
-    private let menuCellHeight: CGFloat = 150
 
     // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpTableView()
+
+        view.addSubview(menuTableView)
+        self.adapter = MenuTableViewAdapter(tableView: self.menuTableView)
+
     }
 
     override func viewDidLayoutSubviews() {
@@ -34,51 +30,6 @@ class MenuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         menuTableView.pin
             .all()
 
-    }
-
-    // MARK: - TableView functions
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-       return 2
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return addCellHeight
-        } else {
-            return menuCellHeight
-        }
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
-            return 5
-            // count events
-        }
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            return tableView.dequeueReusableCell(withIdentifier: Cells.addCell,
-                                                 for: indexPath) as? AddTableViewCell ?? .init()
-        } else {
-            return tableView.dequeueReusableCell(withIdentifier: Cells.menuCell,
-                                                 for: indexPath) as? MenuTableViewCell ?? .init()
-        }
-    }
-
-    // MARK: - Private methods
-
-    private func setUpTableView() {
-        view.addSubview(menuTableView)
-        menuTableView.backgroundColor = .systemGray6
-        menuTableView.separatorStyle = .none
-        menuTableView.register(MenuTableViewCell.self, forCellReuseIdentifier: Cells.menuCell)
-        menuTableView.register(AddTableViewCell.self, forCellReuseIdentifier: Cells.addCell)
-        menuTableView.delegate = self
-        menuTableView.dataSource = self
     }
 
 }
