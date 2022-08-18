87de3e2c684e8661b8a0b63799e12042e44ed662
JuliaAlfimova <juliemoorled@gmail.com>
2022-08-18 22:03:49 +0300

    added labels with images

--- PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
+++ PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
@@ -13,6 +13,7 @@ final class EventVC: UIViewController {
     // MARK: - Private properties
 
     private let eventImageView = UIImageView()
+    private let scrollView = UIScrollView()
     private let eventNameLabel = UILabel()
     private let dateLabel = UILabel()
     private let addressLabel = UILabel()
@@ -29,7 +30,21 @@ final class EventVC: UIViewController {
         super.viewDidLoad()
 
         setUpBackground()
+        setUpEventNameLabel()
+        if let image = UIImage(systemName: dateImageName) {
+            setUpIconWithLabel(icon: image, label: dateLabel,
+                               text: "11pm Monday, 22 August 2022")
+        }
+        if let image = UIImage(systemName: addressImageName) {
+            setUpIconWithLabel(icon: image, label: addressLabel,
+                               text: "New Bar, 17 Lenina street, Sochi")
+        }
+        if let image = UIImage(systemName: priceImageName) {
+            setUpIconWithLabel(icon: image, label: priceLabel,
+                               text: "FREE")
+        }
 
+        // make navbar background invisible
     }
 
     override func viewDidLayoutSubviews() {
@@ -44,14 +59,71 @@ final class EventVC: UIViewController {
         eventImageView.backgroundColor = .systemIndigo
         view.addGradient(firstColor: .systemBackground.withAlphaComponent(0.1),
                          secondColor: .systemBackground)
+        view.addSubview(scrollView)
+    }
+
+    private func setUpEventNameLabel() {
+        scrollView.addSubview(eventNameLabel)
+        eventNameLabel.text = "It's Friday I'm in love"
+        eventNameLabel.font = .systemFont(ofSize: 28, weight: .semibold)
+        eventNameLabel.textColor = .label
+        eventNameLabel.textAlignment = .left
+    }
+
+    private func setUpIconWithLabel(icon: UIImage, label: UILabel, text: String) {
+        let labelText = NSMutableAttributedString()
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = icon.withTintColor(.label)
+        let textAttribute = [ NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .regular),
+                             NSAttributedString.Key.foregroundColor: UIColor.label ]
+        let textAttachment = NSMutableAttributedString(string: text,
+                                                       attributes: textAttribute as [ NSAttributedString.Key: Any ])
+        labelText.append(NSAttributedString(attachment: imageAttachment))
+        labelText.append(NSAttributedString(string: " "))
+        labelText.append(textAttachment)
+        label.attributedText = labelText
+        label.textAlignment = .left
+        scrollView.addSubview(label)
     }
 
     private func setUpLayout() {
+
+        let labelWidth: CGFloat = view.frame.width * 0.8
+        let labelHeight: CGFloat = 24
+
         eventImageView.pin
             .top()
             .left()
             .right()
             .height(view.frame.height/2)
+
+        scrollView.pin
+            .all()
+
+        eventNameLabel.pin
+            .top(eventImageView.frame.maxY - 36)
+            .hCenter()
+            .width(labelWidth)
+            .height(labelHeight)
+
+        dateLabel.pin
+            .top(eventNameLabel.frame.maxY + 12)
+            .hCenter()
+            .width(labelWidth)
+            .height(labelHeight)
+
+        addressLabel.pin
+            .top(dateLabel.frame.maxY + 8)
+            .hCenter()
+            .width(labelWidth)
+            .height(labelHeight)
+
+        priceLabel.pin
+            .top(addressLabel.frame.maxY + 8)
+            .hCenter()
+            .width(labelWidth)
+            .height(labelHeight)
+
     }
 
 }
