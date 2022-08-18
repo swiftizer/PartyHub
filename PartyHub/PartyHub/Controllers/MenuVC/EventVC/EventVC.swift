4466e796d1dc45f41a98a85f77ca9c6da571bffa
JuliaAlfimova <juliemoorled@gmail.com>
2022-08-18 16:48:28 +0300

    added event background

--- PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
+++ PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import PinLayout
 
 final class EventVC: UIViewController {
 
+    // MARK: - Private properties
+
+    private let eventImageView = UIImageView()
+    private let eventNameLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let addressLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let descriptionLabel = UILabel()
+
+    private let dateImageName = "clock"
+    private let addressImageName = "mappin.and.ellipse"
+    private let priceImageName = "dollarsign.circle"
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .systemBackground
+
+        setUpBackground()
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setUpLayout()
+    }
+
+    // MARK: - Private methods
+
+    private func setUpBackground() {
+        view.addSubview(eventImageView)
+        eventImageView.backgroundColor = .systemIndigo
+        view.addGradient(firstColor: .systemBackground.withAlphaComponent(0.1),
+                         secondColor: .systemBackground)
+    }
+
+    private func setUpLayout() {
+        eventImageView.pin
+            .top()
+            .left()
+            .right()
+            .height(view.frame.height/2)
     }
 
 }
--- PartyHub/PartyHub/Other/Extensions/Extensions.swift
+++ PartyHub/PartyHub/Other/Extensions/Extensions.swift
@@ -16,4 +16,16 @@ extension UIView {
         layer.shadowRadius = 2.4
         // TODO: - Будет версия Динара
     }
+
+    func addGradient(firstColor: UIColor, secondColor: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+        gradientLayer.colors = [
+            firstColor.cgColor,
+            secondColor.cgColor,
+            secondColor.cgColor
+        ]
+        self.layer.addSublayer(gradientLayer)
+    }
+
 }
