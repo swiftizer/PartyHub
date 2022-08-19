deff900a87de80c6dcde03cba3c9c99e1dd75ae9
JuliaAlfimova <juliemoorled@gmail.com>
2022-08-19 11:31:07 +0300

    fixed elements

--- PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
+++ PartyHub/PartyHub/Controllers/MenuVC/EventVC/EventVC.swift
@@ -10,6 +10,16 @@ import PinLayout
 
 final class EventVC: UIViewController {
 
+    // MARK: - Internal properties
+
+    var eventName: String = "Event Name"
+    var eventDate: String = "Some date and time"
+    var eventAddress: String = "Somewhere on Earth"
+    var eventDescription: String = "Some description"
+    var eventPrice: Int = 0
+    var participants: Int = 0
+    var eventImage = UIImage()
+
     // MARK: - Private properties
 
     private let eventImageView = UIImageView()
@@ -19,11 +29,13 @@ final class EventVC: UIViewController {
     private let addressLabel = UILabel()
     private let priceLabel = UILabel()
     private let descriptionLabel = UILabel()
-    private let moreButton = UIButton()
+    private let participantsLabel = UILabel()
+    private let mapButton = UIButton(type: .system)
 
     private let dateImageName = "clock"
     private let addressImageName = "mappin.and.ellipse"
     private let priceImageName = "dollarsign.circle"
+    private let participantsImageName = "person.2"
 
     // MARK: - Life cycle
 
@@ -32,22 +44,24 @@ final class EventVC: UIViewController {
 
         setUpBackground()
         setUpEventNameLabel()
-        if let image = UIImage(systemName: dateImageName) {
-            setUpIconWithLabel(icon: image, label: dateLabel,
-                               text: "11pm Monday, 22 August 2022")
-        }
-        if let image = UIImage(systemName: addressImageName) {
-            setUpIconWithLabel(icon: image, label: addressLabel,
-                               text: "New Bar, 17 Lenina street, Sochi")
-        }
-        if let image = UIImage(systemName: priceImageName) {
-            setUpIconWithLabel(icon: image, label: priceLabel,
-                               text: "FREE")
-        }
-        setUpDescriptionLabel()
-        setUpMoreButton()
+        setUpIconWithLabel(label: dateLabel,
+                           iconName: dateImageName,
+                           text: eventDate)
+        setUpIconWithLabel(label: addressLabel,
+                           iconName: addressImageName,
+                           text: eventAddress)
+        setUpIconWithLabel(label: priceLabel,
+                           iconName: priceImageName,
+                           text: (eventPrice == 0) ? "FREE" : "\(eventPrice)")
+        setUpIconWithLabel(label: participantsLabel,
+                           iconName: participantsImageName,
+                           text: "\(participants)")
 
-        // make navbar background invisible and it's elements gray
+        setUpDescriptionLabel()
+        setUpMapButton()
+
+        // make navbar background invisible
+        self.navigationController?.navigationBar.tintColor = .label
         self.navigationController?.navigationBar.isTranslucent = true
 //        navigationController?.navigationBar.isHidden = true
         tabBarController?.tabBar.isHidden = true
@@ -60,41 +74,48 @@ final class EventVC: UIViewController {
 
     // MARK: - Private methods
 
+    @objc private func didTapMapButton() {
+        // open maps
+    }
+
     private func setUpBackground() {
         view.addSubview(eventImageView)
         eventImageView.backgroundColor = .systemIndigo
         view.addGradient(firstColor: .systemBackground.withAlphaComponent(0.1),
                          secondColor: .systemBackground)
         view.addSubview(scrollView)
+        scrollView.showsVerticalScrollIndicator = false
     }
 
     private func setUpEventNameLabel() {
         scrollView.addSubview(eventNameLabel)
-        eventNameLabel.text = "It's Friday I'm in love"
+        eventNameLabel.text = eventName
         eventNameLabel.font = .systemFont(ofSize: 28, weight: .semibold)
         eventNameLabel.textColor = .label
         eventNameLabel.textAlignment = .left
     }
 
-    private func setUpIconWithLabel(icon: UIImage, label: UILabel, text: String) {
-        let labelText = NSMutableAttributedString()
-        let imageAttachment = NSTextAttachment()
-        imageAttachment.image = icon.withTintColor(.label)
-        let textAttribute = [ NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .regular),
-                             NSAttributedString.Key.foregroundColor: UIColor.label ]
-        let textAttachment = NSMutableAttributedString(string: text,
-                                                       attributes: textAttribute as [ NSAttributedString.Key: Any ])
-        labelText.append(NSAttributedString(attachment: imageAttachment))
-        labelText.append(NSAttributedString(string: " "))
-        labelText.append(textAttachment)
-        label.attributedText = labelText
-        label.textAlignment = .left
-        scrollView.addSubview(label)
+    private func setUpIconWithLabel(label: UILabel, iconName: String, text: String) {
+        if let icon = UIImage(systemName: iconName) {
+            let labelText = NSMutableAttributedString()
+            let imageAttachment = NSTextAttachment()
+            imageAttachment.image = icon.withTintColor(.label)
+            let textAttribute = [ NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .regular),
+                                 NSAttributedString.Key.foregroundColor: UIColor.label ]
+            let textAttachment = NSMutableAttributedString(string: text,
+                                                           attributes: textAttribute as [ NSAttributedString.Key: Any ])
+            labelText.append(NSAttributedString(attachment: imageAttachment))
+            labelText.append(NSAttributedString(string: " "))
+            labelText.append(textAttachment)
+            label.attributedText = labelText
+            label.textAlignment = .left
+            scrollView.addSubview(label)
+        }
     }
 
     private func setUpDescriptionLabel() {
         scrollView.addSubview(descriptionLabel)
-        descriptionLabel.text = "Самая вайбовая вечериночка лета, наполненная инди-музыкой и самыми приятными людьми! Для вашей безопасности на входе действует фейс контроль. "
+        descriptionLabel.text = eventDescription
         descriptionLabel.font = .systemFont(ofSize: 17, weight: .regular)
         descriptionLabel.textColor = .label
         descriptionLabel.textAlignment = .left
@@ -102,13 +123,13 @@ final class EventVC: UIViewController {
         descriptionLabel.lineBreakMode = .byWordWrapping
     }
 
-    private func setUpMoreButton() {
-        scrollView.addSubview(moreButton)
-        moreButton.backgroundColor = .systemGray6
-        moreButton.setTitle("Read more", for: .normal)
-        moreButton.layer.cornerRadius = 15
-        moreButton.addTarget(self, action: #selector(didTapMoreButton), for: .touchUpInside)
-        // change color while highlighted
+    private func setUpMapButton() {
+        scrollView.addSubview(mapButton)
+        mapButton.backgroundColor = .systemGray5
+        mapButton.setTitle("Show on map", for: .normal)
+        mapButton.layer.cornerRadius = 15
+        mapButton.addTarget(self, action: #selector(didTapMapButton), for: .touchUpInside)
+        mapButton.tintColor = .label
     }
 
     private func setUpLayout() {
@@ -124,48 +145,41 @@ final class EventVC: UIViewController {
 
         scrollView.pin
             .all()
-        // scrollView не работает?
 
         eventNameLabel.pin
-            .top(eventImageView.frame.maxY - 36)
+            .top(eventImageView.frame.height - 36)
             .hCenter()
             .width(labelWidth)
             .height(labelHeight)
 
-        dateLabel.pin
-            .top(eventNameLabel.frame.maxY + 12)
-            .hCenter()
-            .width(labelWidth)
-            .height(labelHeight)
+        let labelsWithImages = [dateLabel, addressLabel, priceLabel, participantsLabel]
+        var labelTop = eventNameLabel.frame.maxY + 12
+        for label in labelsWithImages {
+            label.pin
+                .top(labelTop)
+                .hCenter()
+                .width(labelWidth)
+                .height(labelHeight)
+            labelTop += labelHeight
+            labelTop += 8
+        }
 
-        addressLabel.pin
-            .top(dateLabel.frame.maxY + 8)
-            .hCenter()
-            .width(labelWidth)
-            .height(labelHeight)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: participantsLabel.bottomAnchor, constant: 12),
+            descriptionLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            descriptionLabel.widthAnchor.constraint(equalTo: participantsLabel.widthAnchor)
+        ])
 
-        priceLabel.pin
-            .top(addressLabel.frame.maxY + 8)
-            .hCenter()
-            .width(labelWidth)
-            .height(labelHeight)
-
-        descriptionLabel.pin
-            .top(priceLabel.frame.maxY - 12)
-            .hCenter()
-            .width(labelWidth)
-            .height(150) // resize
-
-        moreButton.pin
+        mapButton.pin
             .top(descriptionLabel.frame.maxY + 12)
             .hCenter()
             .width(labelWidth)
             .height(50)
 
-    }
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width,
+                                        height: UIScreen.main.bounds.height + descriptionLabel.frame.height - 300)
 
-    @objc private func didTapMoreButton() {
-        // open some url
     }
 
 }
